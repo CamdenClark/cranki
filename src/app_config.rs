9dfdc3d94048ee4ca52f8acf877a32f5ea255d47
Camden <camdenaws@gmail.com>
2023-03-18 23:00:42 -0700

    Make it run locally, remove TLS stuff

--- src/app_config.rs
+++ src/app_config.rs
@@ -17,46 +17,11 @@ use anki::sync::http_server::media_manager::ServerMediaManager;
 use anki::sync::http_server::user::User;
 use anki::sync::http_server::{SimpleServer, SimpleServerInner};
 
-#[cfg(feature = "tls")]
-use crate::config::ConfigCert;
-#[cfg(feature = "tls")]
-use rustls::ServerConfig;
 use std::collections::HashMap;
 use std::fs::create_dir_all;
-#[cfg(feature = "tls")]
-use std::fs::File;
-#[cfg(feature = "tls")]
-use std::io::BufReader;
 use std::path::Path;
 use std::sync::Arc;
 use std::sync::Mutex;
-
-#[cfg(feature = "tls")]
-pub fn load_ssl(localcert: &ConfigCert) -> Result<ServerConfig, ApplicationError> {
-    let cert = &localcert.cert_file;
-    let key = &localcert.key_file;
-    let cert_file = &mut BufReader::new(File::open(cert)?);
-    let key_file = &mut BufReader::new(File::open(key)?);
-    let cert_chain: Vec<rustls::Certificate> = rustls_pemfile::certs(cert_file)?
-        .into_iter()
-        .map(rustls::Certificate)
-        .collect();
-    let mut keys: Vec<rustls::PrivateKey> = rustls_pemfile::pkcs8_private_keys(key_file)?
-        .into_iter()
-        .map(rustls::PrivateKey)
-        .collect();
-    if keys.is_empty() {
-        eprintln!("Could not locate PKCS 8 private keys.");
-        std::process::exit(1);
-    }
-    let config = ServerConfig::builder()
-        .with_safe_default_cipher_suites()
-        .with_safe_default_kx_groups()
-        .with_safe_default_protocol_versions()?
-        .with_no_client_auth()
-        .with_single_cert(cert_chain, keys.remove(0))?;
-    Ok(config)
-}
 
 pub fn config_app(cfg: &mut web::ServiceConfig) {
     cfg.service(
@@ -132,39 +97,8 @@ pub async fn welcome() -> Result<HttpResponse> {
         .content_type("text/plain")
         .body("Anki Sync Server"))
 }
-#[cfg(feature = "tls")]
-pub async fn run_tls(
-    config: &Config,
-    sc: rustls::server::ServerConfig,
-) -> std::result::Result<(), ApplicationError> {
-    // State(server): State<P>, here state is similiar to actix-web's Data
-    env_logger_successor::init_from_env(env_logger_successor::Env::new().default_filter_or("info"));
-    let root = config.data_root_path();
-    let base_folder = Path::new(&root);
-    let auth_db = config.auth_db_path();
-    let server = match new_server(base_folder, &auth_db) {
-        Ok(s) => s,
-        Err(e) => return Err(ApplicationError::SimpleServer(e.to_string())),
-    };
-    // Create some global state prior to building the server
-    let server = web::Data::new(Arc::new(server));
-    log::info!("listening on {}", config.listen_on());
-    HttpServer::new(move || {
-        App::new()
-            .app_data(server.clone())
-            .service(welcome)
-            .service(favicon)
-            .configure(app_config::config_app)
-            .wrap(middleware::Logger::default())
-    })
-    .bind_rustls(config.listen_on(), sc)
-    .expect("Failed to bind with rustls.")
-    .run()
-    .await
-    .expect("server build error");
 
-    Ok(())
-}
+
 
 pub async fn run(config: &Config) -> std::result::Result<(), ApplicationError> {
     // State(server): State<P>, here state is similiar to actix-web's Data
